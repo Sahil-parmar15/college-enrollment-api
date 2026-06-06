ba01913499754e1a3d22ce7bbb53f3eec60eb277
Sahil-parmar15 <sahilparmar1504@gmail.com>
2026-06-06 12:22:23 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { AdminsModule } from './admins/admins.module';
+import { StudentsModule } from './students/students.module';
+import { CoursesModule } from './courses/courses.module';
+import { EnrollmentsModule } from './enrollments/enrollments.module';
+
+@Module({
+  imports: [AuthModule, AdminsModule, StudentsModule, CoursesModule, EnrollmentsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
